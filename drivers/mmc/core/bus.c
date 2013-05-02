90bb3ace47d024ec952a994f910f4170cf70e301
Ulf Hansson <ulf.hansson@linaro.org>
2013-05-02 14:02:37 +0200

    mmc: core: Add bus_ops for runtime pm callbacks
    SDIO is the only protocol that uses runtime pm for the card device
    right now. To provide the option for sd and mmc to use runtime pm as
    well the bus_ops callback are extended with two new functions. One for
    runtime_suspend and one for runtime_resume.
    
    This patch will also implement the callbacks for SDIO to make sure
    existing functionality is maintained. It also prepares to move
    away from using the mmc_power_restore_host API, since it is not
    needed when using runtime PM.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/bus.c
+++ drivers/mmc/core/bus.c
@@ -171,29 +171,25 @@ static int mmc_bus_resume(struct device *dev)
 static int mmc_runtime_suspend(struct device *dev)
 {
 	struct mmc_card *card = mmc_dev_to_card(dev);
+	struct mmc_host *host = card->host;
+	int ret = 0;
 
-	if (mmc_use_core_runtime_pm(card->host)) {
-		/*
-		 * If idle time bkops is running on the card, let's not get
-		 * into suspend.
-		 */
-		if (mmc_card_doing_bkops(card) && mmc_card_is_prog_state(card))
-			return -EBUSY;
-		else
-			return 0;
-	} else {
-		return mmc_power_save_host(card->host);
-	}
+	if (host->bus_ops->runtime_suspend)
+		ret = host->bus_ops->runtime_suspend(host);
+
+	return ret;
 }
 
 static int mmc_runtime_resume(struct device *dev)
 {
 	struct mmc_card *card = mmc_dev_to_card(dev);
+	struct mmc_host *host = card->host;
+	int ret = 0;
 
-	if (mmc_use_core_runtime_pm(card->host))
-		return 0;
-	else
-		return mmc_power_restore_host(card->host);
+	if (host->bus_ops->runtime_resume)
+		ret = host->bus_ops->runtime_resume(host);
+
+	return ret;
 }
 
 static int mmc_runtime_idle(struct device *dev)
