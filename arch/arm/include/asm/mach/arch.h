6fa83d3c0eee51cfc93e43c0fdfb1eaefe046d60
Jon Medhurst <tixy@linaro.org>
2013-05-21 13:40:51 +0000

    ARM: Enable selection of SMP operations at boot time
    Add a new 'smp_init' hook to machine_desc so platforms can specify a
    function to be used to setup smp ops instead of having a statically
    defined value.  The hook must return true when smp_ops are initialized.
    If false the static mdesc->smp_ops will be used by default.
    
    Add the definition of "bool" by including the linux/types.h file to
    asm/mach/arch.h and make it self-contained.
    
    Signed-off-by: Jon Medhurst <tixy@linaro.org>
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/include/asm/mach/arch.h
+++ arch/arm/include/asm/mach/arch.h
@@ -8,6 +8,8 @@
  * published by the Free Software Foundation.
  */
 
+#include <linux/types.h>
+
 #ifndef __ASSEMBLY__
 #include <linux/reboot.h>
 
@@ -16,8 +18,10 @@ struct pt_regs;
 struct smp_operations;
 #ifdef CONFIG_SMP
 #define smp_ops(ops) (&(ops))
+#define smp_init_ops(ops) (&(ops))
 #else
 #define smp_ops(ops) (struct smp_operations *)NULL
+#define smp_init_ops(ops) (bool (*)(void))NULL
 #endif
 
 struct machine_desc {
@@ -41,7 +45,9 @@ struct machine_desc {
 	unsigned char		reserve_lp2 :1;	/* never has lp2	*/
 	enum reboot_mode	reboot_mode;	/* default restart mode	*/
 	struct smp_operations	*smp;		/* SMP operations	*/
-	void			(*fixup)(struct tag *, char **);
+	bool			(*smp_init)(void);
+	void			(*fixup)(struct tag *, char **,
+					 struct meminfo *);
 	void			(*reserve)(void);/* reserve mem blocks	*/
 	void			(*map_io)(void);/* IO mapping function	*/
 	void			(*init_early)(void);
